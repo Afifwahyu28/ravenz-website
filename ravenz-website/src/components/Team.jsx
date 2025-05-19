9b7ad744568b35c1e7de83f6429dea3e2cc6e7bf
Afifwahyu28 <nasaorgagro@gmail.com>
2025-05-19 16:23:45 +0700

    penambahan data mahasiswa

---
+++ ravenz-website/src/components/Team.jsx
@@ -0,0 +1,34 @@
+function Team() {
+  return (
+    <>
+      <section style={{ textAlign: "center", margin: "20px 0" }}>
+        <h2>Informasi Mahasiswa</h2>
+        <div>
+          <p>
+            <strong>Nama:</strong> Afif Wahyu Adhitya
+          </p>
+          <p>
+            <strong>NIM:</strong> 22.11.4991
+          </p>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <p>
+            <strong>Nama:</strong> Muhammad Eka Karunia Ramadhan
+          </p>
+          <p>
+            <strong>NIM:</strong> 22.11.4978
+          </p>
+          <p>
+          <strong>Nama:</strong> Mahsa Athallah Zufar
+          </p>
+            <strong>NIM:</strong> 22.11.4969
+          <p>
+            <strong>Mata Kuliah:</strong> Digital Bisnis
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Team;
